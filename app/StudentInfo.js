5e6ca32382c8bb77ead6c6715c74788c96be0761
minji <minji.kim02@edu.sait.ca>
2023-09-14 16:03:09 -0600

    week2
    add and change studentinfo.js, page.js

---
+++ app/StudentInfo.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+    return (
+        <main>
+            <p>Minji Kim</p>
+            <p>CPRG 306 F</p>
+            <Link href="https://github.com/MinjiKim02/cprg306-assignments">https://github.com/MinjiKim02/cprg306-assignments</Link>
+        </main>
+    );
+
+}
---
+++ app/week2/page.js
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+import StudentInfo from '../StudentInfo';
+
+
+export default function Home() {
+  return (
+    <main>
+      <h1>My Shopping List</h1>
+      <StudentInfo />
+    </main>
+  );
+}
